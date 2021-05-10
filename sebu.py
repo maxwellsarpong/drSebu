397a44ec0ea7996d3e72461623b8d69ae1893dae
Big-Max <maxwellsarpong07@gmail.com>
2021-05-10 00:07:24 +0000

    initial commit

--- sebu.py
+++ sebu.py
@@ -1,23 +1,51 @@
 import datetime
-from dateutil import parser
-from datetime import datetime
+import csv
 
 def pay_calculator():
-    
-    date_time_started = parser.parse(input('Please input the date and time started. Format yyyy-mm-dd HH:MM : \n '))
-    date_time_ended = parser.parse(input('Please input the time started. Format HH:MM: \n '))
-  
+
+    print("Hourly Calculator\n")
+    date_started = str(input('Please input the date started. Format yyyy-mm-dd : '))
+    time_started = input('specify time (24 hrs)started in HH:MM format: ').split(":")
+    date_ended = str(input('Please input the date ended. Format yyyy-mm-dd: '))
+    time_ended = input('specify time (24 hrs) ended in HH:MM format: ').split(":")
+
+    ts = (":").join(time_started)
+    te = (":").join(time_ended)
+      
 
     # calculations
+    time_s = [int(i) for i in time_started ]
 
-    duration_date =  date_time_ended - date_time_started
+    time_e = [int(i) for i in time_ended]
 
-    if duration_date:
-        money_earned = duration_date * 5
-        print(f"money earend : {duration_date}")
-        print(duration_date)
+    a = 60*time_s[0] + time_s[1]
+    b = 60*time_e[0] + time_e[1]
+
+    ans = b - a
+    hourly_pay = 5
+    if ans > 0:
+        val = ans/60
+        money_earned = val* hourly_pay
+
+        print("the hourly money earned: GH$", money_earned, "cedis")
+
+        header = ['Date Started', 'Time Started', 'Date Ended', 'Time Ended', 'Amount Earned']
+        data = [date_started , ts, date_ended, te, money_earned]
+        
+        with open('Sebu/moneyeEarned2.csv', 'w', newline='',encoding='UTF8') as f:
+            writer = csv.writer(f)
+
+            # write the header
+            writer.writerow(header)
+
+            # write the data
+            writer.writerow(data)
+
+            f.close()
+                  
     else:
-        print("duration less than 0 hence no income")
+        print("hours cannot be less than zero")
+
 
 
 pay_calculator()
